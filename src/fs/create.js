079c92c9ccae71bc40cf695bc0c297ab699148d2
Arseniy <krutoi.arseniy@gmail.com>
2025-10-08 22:32:17 +0300

    feat: add script in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from "node:fs";
+
 const create = async () => {
-  // Write your code here
+  fs.writeFile(
+    "src/fs/files/fresh.txt",
+    "I am fresh and young",
+    "utf8",
+    (err) => {
+      if (err) {
+        throw new Error("Error");
+      }
+    }
+  );
+  try {
+    const exists = fs.existsSync("src/fs/files/fresh.txt");
+    if (exists) {
+      console.log("FS operation failed");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await create();
