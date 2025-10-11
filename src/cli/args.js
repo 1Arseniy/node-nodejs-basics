c9a4e1adbd279bc037b02888d3b0a51040c740e8
Arseniy <krutoi.arseniy@gmail.com>
2025-10-11 11:52:58 +0300

    feat: add env.js, args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,29 @@
+import process from "node:process";
+
 const parseArgs = () => {
-  // Write your code here
+  const arr = [];
+  try {
+    const allVariables = process.argv.slice(2);
+    // console.log(variables.);
+    for (let i = 0; i < allVariables.length; i += 2) {
+      // if (allVariables[i].startsWith("--")) {
+      //   const a = allVariables[i].slice(2);
+      //   console.log(a);
+      // }
+      arr.push(allVariables.slice(i, i + 2));
+      // const format = allVariables[i].
+      // if (i % 2 !== 0) {
+      // const formatKeys = allVariables
+      // arr.push(
+      //   allVariables.
+      //   // .join(" is ")
+      // );
+      // }
+    }
+  } catch {
+    console.log("process operation failed");
+  }
+  console.log(arr);
 };
 
 parseArgs();
