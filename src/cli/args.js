2748af9ecd3da226e46b63670f9044a17fd1e249
Arseniy <krutoi.arseniy@gmail.com>
2025-10-12 13:47:38 +0300

    feat: add calcHash, update esm.js, args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,26 +4,19 @@ const parseArgs = () => {
   const arr = [];
   try {
     const allVariables = process.argv.slice(2);
-    // console.log(variables.);
+    const formatArr = allVariables.map((str) => {
+      if (str.startsWith('--')) {
+        return str.slice(2)
+      }
+      return str
+    })
     for (let i = 0; i < allVariables.length; i += 2) {
-      // if (allVariables[i].startsWith("--")) {
-      //   const a = allVariables[i].slice(2);
-      //   console.log(a);
-      // }
-      arr.push(allVariables.slice(i, i + 2));
-      // const format = allVariables[i].
-      // if (i % 2 !== 0) {
-      // const formatKeys = allVariables
-      // arr.push(
-      //   allVariables.
-      //   // .join(" is ")
-      // );
-      // }
+      arr.push(formatArr.slice(i, i + 2).join(' is '));
     }
+    console.log(arr.join(', '));
   } catch {
     console.log("process operation failed");
   }
-  console.log(arr);
 };
 
 parseArgs();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { pipeline } from 'node:stream/promises';
+import fs from 'fs'
+
+const { createHmac } = await import('node:crypto');
+
 const calculateHash = async () => {
-  // Write your code here
+  const secret = 'GfG';
+  const hash = createHmac('sha256', secret)
+  await pipeline(
+    fs.createReadStream('src/hash/files/fileToCalculateHashFor.txt'), 
+    async function* (source) {
+      for await(const chunk of source) {
+       hash.update(chunk)
+      }
+      const result = hash.digest('hex')
+      console.log(result)
+    }
+  ).catch(() => console.log('calculateHash operation failed'))
 };
 
 await calculateHash();
