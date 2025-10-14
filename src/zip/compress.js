41bce9fc5b66587cafed8fbc3a0b3042589ad0fc
Arseniy <krutoi.arseniy@gmail.com>
2025-10-14 23:06:09 +0300

    feat: add compress.js, decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import fs from "fs";
+import { unlink } from "fs/promises";
+
 const compress = async () => {
-  // Write your code here
+  const gzib = createGzip();
+  const readStream = fs.createReadStream("src/zip/files/fileToCompress.txt");
+  const writeStream = fs.createWriteStream("src/zip/files/archive.gz");
+  try {
+    await pipeline(readStream, gzib, writeStream);
+    await unlink("src/zip/files/fileToCompress.txt");
+  } catch {
+    console.log("compress operation failed");
+  }
 };
 
 await compress();
