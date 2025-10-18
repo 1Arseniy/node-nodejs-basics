3b1f39fa61dbcc8c4a20940c995f3b5924066b54
Arseniy <krutoi.arseniy@gmail.com>
2025-10-18 12:28:04 +0300

    feat: update main.js, worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import { Worker, workerData, parentPort, isMainThread } from "worker_threads";
+import { fileURLToPath } from "node:url";
+import os from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+
 const performCalculations = async () => {
-  // Write your code here
+  // console.log(workerData);
+  if (isMainThread) {
+    let number = 10;
+    for (let i = 0; i < os.cpus().length; i++) {
+      number++;
+      const worker = new Worker(__filename, {
+        workerData: number,
+      });
+      worker.on("message", (data) => console.log(data));
+      worker.on("error", () => console.log("err"));
+    }
+  }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
+import { Worker, isMainThread, workerData } from "node:worker_threads";
+import { parentPort } from "worker_threads";
+import { fileURLToPath } from "node:url";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const __filename = fileURLToPath(import.meta.url);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
+  // parentPort.on('message')
+  if (!isMainThread) {
+    // parentPort.on("message", () => {
+    const result = nthFibonacci(workerData);
+    parentPort.postMessage(result);
+    // });
+  }
 };
 
 sendResult();
